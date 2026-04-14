d0533dd159cd320020e82b7236a0b09331e765cd
vjj9 <vjayjaina@gmail.com>
2026-04-13 20:59:22 -0500

    Additional Changes

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({mode}) => {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
+      preserveSymlinks: true,
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
