d72e8fa37f86a768f83eb619fa389937a156d1a1
vjj9 <vjayjaina@gmail.com>
2026-04-14 22:48:21 -0500

    Additional Changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.planovakidz.app',
-  appName: 'Planova Kidz',
+  appId: 'com.smartplanner.kids',
+  appName: 'Smart Planner For Kids',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
